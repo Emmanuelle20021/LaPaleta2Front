059671338981f5f47bea4d80c9b455150267e2fd
Emma Mora <emmanu20021@gmail.com>
2023-12-02 17:05:06 -0600

    Componentes en el login

--- src/components/input_form/input.jsx
+++ src/components/input_form/input.jsx
@@ -1,11 +1,14 @@
 import './input.scss';
 
 export function Input({ children, type, name, id, placeholder, pattern}){
+    const classNames = children? 'input-icon-div icon-true' : 'input-icon-div';
     return <>
-        <label htmlFor={name} className='form-label'>
-            <p className='label-p'>{placeholder}</p>
-            <div className='input-icon-div'>
+        <label form='form-login' className='form-label' id={id + '-label'}>
+            <div className='icon-aling'>
                 {children}
+            </div>
+            <div className={classNames}>
+                <p className='label-p'>{placeholder}</p>
                 <input className='form-input' placeholder={placeholder} type={type} name={name} id={id} pattern={pattern}/>
             </div>
         </label>
--- src/modules/login/login.jsx
+++ src/modules/login/login.jsx
@@ -1,23 +1,43 @@
 import './login.scss'
 import { Input } from '../../components/input_form/input'
+import { MdOutlineAlternateEmail } from "react-icons/md";
+import { FaLock } from "react-icons/fa";
+import Footer from '../../components/footer/footer';
+import Navbar from '../../components/navbar/navbar';
 
 const phonePattern = "[0-9]{3}(-?)[0-9]{3}(-?)[0-9]{3}"
 
 export function Login(){
     return <div className="page grid-login">
-        <aside className="image-side">
+        <div className='login-nav'>
+            <Navbar></Navbar>
+        </div>
+        <aside className="image-side padding">
             <div className='img-circle big-img'>
                 <img className='image' src="https://eminusapi.uv.mx/eminusapi//profiles/RHvLQb5vwmBmkNKJl739g.png" alt="image name" />
             </div>
         </aside>
-        <aside className="login-container">
-            <form action="" className='form'>
+        <aside className="login-container padding">
+            <form action="#" className='form' id='form-login'>
                 <p className="form-title">Bienvenido</p>
-                <Input id="email-input-login" name="email-input" placeholder="Correo Electrónico" type="email"></Input>
-                <Input id="phone-input-login" name="phone-input" placeholder="Teléfono" type="tel" pattern={phonePattern}></Input>
-                <Input id="password-input-login" name="password-input" placeholder="Contraseña" type="password"></Input>
+                <Input id="email-input-login" name="email-input" placeholder="Correo Electrónico" type="email">
+                    <MdOutlineAlternateEmail className='input-icon'/>
+                </Input>
+                <Input id="password-input-login" name="password-input" placeholder="Contraseña" type="password">
+                    <FaLock className='input-icon' />
+                </Input>
+                <div className='form-link'>
+                    <a href="#">Olvide mi contraseña</a>
+                </div>
+                <div className='form-link margin-top'>
+                    <p>No tienes una cuenta?</p>
+                    <a href="#">Registrate</a>
+                </div>
                 <button type="submit" className='form-submit-button'>Aceptar</button>
             </form>
         </aside>
+        <footer className='login-footer' >
+            <Footer></Footer>
+        </footer>
     </div>
 }
